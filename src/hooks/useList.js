30ab667b45042ea7a23ebe9d5de2f92e203e8038
dalejandroramirez <dalejandro.ramirez@udea.edu.co>
2022-12-19 23:26:35 -0500

    feat: Mode dark

--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -1,23 +1,50 @@
 import { useState } from 'react';
 import { render } from 'react-dom';
 
+/**
+ * 
+ * @param {anyArray} initialValue  
+ * @returns Un Hook personalizado para crear una lista
+ */
+
 const useList = (initialValue = []) => {
   const [value, setValue] = useState(initialValue);
 
-  // Push new value to list
+
+  
+/**
+ * Push new value to list
+ * @param {string} element 
+ */
   const push = (element) => {
     setValue((oldValue) => [...oldValue, element]);
   };
 
-  // Remove Value from list
+  /**
+   * Remove Value from list en la posicion index
+   * @param {number} index 
+   */
   const remove = (index) => {
     setValue((oldValue) => oldValue.filter((_, i) => i !== index));
   };
 
-  // List is Empty ? true/ false
+
+  /** 
+   * List is Empty ? true/ false
+   * @returns {boolean} true if is empty 
+   */
   const isEmpty = () => value.length === 0;
 
+  /**
+   * 
+   * @returns {number} length of list
+   */
+
   const lenList = () => value.length;
+
+  /**
+   * Reset of list
+   */
 
   const removeList = () => {
     setValue([]);
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
-const useLocalStorege = ({ defaultValue = '', key = 'config' }) => {
+const useLocalStorege = (key, defaultValue = null) => {
+  // const key = "config"
   const [value, setValue] = useState(() => {
     try {
       const item = localStorage.getItem(key);
